bf72972c868aa771557719b9538863bbbeb8c706
Shahed <shahda2011@yahoo.com>
2023-06-19 20:25:34 +0300

    Assignment2

---
+++ app/src/main/java/com/example/kidsmulgame/ui/ChooseActivity.java
@@ -0,0 +1,55 @@
+package com.example.kidsmulgame.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kidsmulgame.R;
+
+public class ChooseActivity extends AppCompatActivity {
+    private TextView textView2;
+    private TextView textView3;
+    private TextView textView5;
+    private ImageView imageView;
+    private ImageView imageView2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose);
+
+        extracted();
+    }
+
+    private void extracted() {
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        textView5 = findViewById(R.id.textView5);
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+
+
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseActivity.this, MultiplyActivity.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        });
+
+
+    }
+
+
+}
